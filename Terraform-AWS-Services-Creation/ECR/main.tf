1f2a12bc4db586b855c97020c94ab018537f363d
johanmn <johanmoonien@gmail.com>
2021-09-09 10:53:40 +0400

    add code for ECR and EKS

--- Terraform-AWS-Services-Creation/ECR/main.tf
+++ Terraform-AWS-Services-Creation/ECR/main.tf
@@ -17,9 +17,12 @@ provider "aws" {
 
 resource "aws_ecr_repository" "devopsthehardway-ecr-repo" {
   name                 = var.repo_name
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
+  }
+  encryption_configuration {
+    encryption_type = "KMS"
   }
 }
